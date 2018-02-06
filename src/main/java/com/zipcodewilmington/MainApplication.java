c0647da1b8df7f5e9a2ba893351fb42f4fd9a287
Joshua Wurdemann <joshuawurdemann@zipcoders-MacBook-Pro-9.local>
2018-02-06 15:45:32 -0500

    first commit

---
+++ src/main/java/com/zipcodewilmington/MainApplication.java
@@ -0,0 +1,19 @@
+package com.zipcodewilmington;
+
+/**
+ *  Joshua Wurdemann 02/06/18
+ */
+public class MainApplication {
+
+    private String hello = "HelloWorld";
+
+    public static void main(String[] args) {
+
+
+
+
+
+
+        System.out.println(hello);
+    }
+}
